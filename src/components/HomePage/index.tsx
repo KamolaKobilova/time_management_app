0fcc694c9e2d7df9de096f86727d331f3e7efb89
Kamola Kobilova <kamola_8880@mail.ru>
2023-12-21 15:17:22 +0500

    add

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -7,7 +7,8 @@ function index() {
              <div className="text">
                 <h2>Your life's work, <br /> powered by our life's work</h2>
                  <p>A unique and powerful software suite to transform the <br /> way you work. Designed for businesses of all sizes, built <br /> by a company that values your privacy </p>
-                <button>Get Started For Free <img src="" alt="" /> </button>
+                <button>Get Started For Free <br />
+                <img src="" alt="" /> </button>
              </div>
           </div>
       </div>
